da28f1c1f58f2727905484ccf39f03ba4cf2bd09
Damon Timm <damontimm@gmail.com>
2017-10-10 08:34:56 -0700

    might as well update these

--- dotfiles/config/fish/aliases.fish
+++ dotfiles/config/fish/aliases.fish
@@ -5,6 +5,7 @@ alias ssh.kf 'ssh kf@site1'
 alias ssh.rosalee 'ssh rosalee@site1'
 alias ssh.nciec 'ssh nciec@site1'
 alias ssh.pai 'ssh pai@site1'
+alias ssh.diinst 'ssh diinst@site1'
 
 # ruby
 alias chruby.lb 'chruby 2.3.0'
@@ -18,12 +19,16 @@ alias seed_migrate='bundle exec rake seed:migrate'
 # linguabee
 alias lb.app 'chruby 2.3.0; and cd ~/Documents/linguabee/linguabee-app; and bundle exec rails server -p 3000'
 alias lb.hive 'chruby 2.3.0; and cd ~/Documents/linguabee/linguabee-hive; and bundle exec rails server -p 3002'
-alias lb.skep 'chruby 2.3.0; and cd ~/Documents/linguabee/linguabee-skep; and bundle exec rails server -p 3003'
+alias lb.skep 'chruby 2.3.0; and cd ~/Documents/linguabee/linguabee-skep; and bundle exec rails server -p 3004'
 alias lb.hive.localhost 'chruby 2.3.0; and cd ~/Documents/linguabee/linguabee-hive; and bundle exec rails server --binding=0.0.0.0 -p 3002'
 alias lb.test.reset 'psql linguabee_api_test -c "drop schema public cascade; create schema public;"; and bundle exec rake db:migrate RAILS_ENV=test; and bundle exec rake db:seed:base RAILS_ENV=test'
 alias lb.reset.develop 'psql linguabee_api_development -c "drop schema public cascade; create schema public;"'
 alias lb.create.migration 'bundle exec rails g migration'
-
+alias lb.kill.app 'kill -9 (cat /Users/damon/Documents/linguabee/linguabee-app/tmp/pids/server.pid)'
+alias lb.kill.hive 'kill -9 (cat /Users/damon/Documents/linguabee/linguabee-hive/tmp/pids/server.pid)'
+alias lb.kill.skep 'kill -9 (cat /Users/damon/Documents/linguabee/linguabee-skep/tmp/pids/server.pid)'
+alias lb.backup_api 'heroku pg:backups capture -a api-linguabee; heroku pg:backups -a api-linguabee public-url | cat'
+alias lb.backup_url 'heroku pg:backups -a api-linguabee public-url | cat'
 # from chad
 # alias backup_dl 'curl -o latest.dump'
 # alias wipedb 'psql linguabee_api_development -c "drop schema public cascade; create schema public;"'
@@ -57,7 +62,7 @@ function lb.api
     set -g db_name "linguabee_api_$argv[1]"
   end
   # set env variable for database.yml
-  set -g -x FLIP_TABLE $db_name
+  set -g -x DATABASE_URL $db_name
   echo "USING DATABASE: $db_name"
   chruby 2.3.0; and cd ~/Documents/linguabee/linguabee-api; and bundle exec rails server -p 3001
 end
@@ -72,20 +77,19 @@ end
 function lb.update_db
   if test (count $argv) -eq 2
     set -g db_name "linguabee_api_$argv[1]"
-    curl -o latest.dump $argv[2]
   else if test (count $argv) -eq 1
     set -g db_name "linguabee_api_production"
-    curl -o latest.dump $argv[1]
-  else
-    echo "using the old latest.dump file as no arguments present"
   end
 
   echo "lb.update_db: running DB: $db_name"
 
-  set -g -x FLIP_TABLE $db_name
+  set -g -x DATABASE_URL $db_name
+  heroku pg:backups:capture -a api-linguabee
+  curl -o latest.dump (heroku pg:backups:url -a api-linguabee)
   psql $db_name -c "drop schema public cascade; create schema public;"
   pg_restore --verbose --clean --no-acl --no-owner -h localhost -U damon -d $db_name latest.dump
   bundle exec rake db:mask_emails
+  bundle exec rake db:seed:base
 end
 
 function ffmpeg.videocopy
@@ -97,3 +101,5 @@ function ffmpeg.videocopy.ac3
     echo "COPYING VIDEO, TRANSCODING AUDIO"
     ffmpeg -i $argv[1] -map 0 -vcodec copy -c:a ac3 -b:a 160k $argv[2]
 end
+
+# ffmpeg -i DYUSA_SystematicOppression.mp4 -vf subtitles=DYUSA_SystematicOppression.en.vtt DYUSA_SystematicOppression.Subs.mp4
